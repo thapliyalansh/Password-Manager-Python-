bf1bbbf7f0c671e47c3107cc997838f1a2520f7f
thapliyalansh <91578700+thapliyalansh@users.noreply.github.com>
2022-01-13 18:01:56 +0530

    descriptive commet for code changes

---
+++ Password Keeper/main.py
@@ -0,0 +1,30 @@
+
+# This is the main file for the Password Keeper Program.
+
+# Import main modules
+import sys
+
+sys.path.insert(0, 'res')
+sys.path.insert(0, 'bin')
+
+# Import app modules
+import f
+
+
+#Log the startup
+f.LogStartUp()
+
+# Check if the modules work or not.
+print("Application started.")
+
+# 0n the launch, we will check if the application was already launched or not by viewing our data fileset.
+# TODO: Install the res folder along with the files, and empty bin folder
+
+if(not f.checkFileExists("bin/gp.txt")):
+    f.Log("This is the first launch.","mainFile")
+    import s
+    s.showWindow()
+else:
+    f.Log("Data found","mainFile")
+    import m
+    m.showWindow()
---
+++ Password Management System/main.py
@@ -0,0 +1,29 @@
+#main file 
+
+# Import main modules
+import sys
+
+sys.path.insert(0, 'res')
+sys.path.insert(0, 'bin')
+
+# Import app modules
+import f
+
+
+#Log the startup
+f.LogStartUp()
+
+# Check if the modules work or not.
+print("Application started.Password Management System")
+
+# 0n the launch, we will check if the application was already launched or not by viewing our data fileset.
+# TODO: Install the res folder along with the files, and empty bin folder
+
+if(not f.checkFileExists("bin/gp.txt")):
+    f.Log("This is the first launch.","mainFile")
+    import s
+    s.showWindow()
+else:
+    f.Log("Data found","mainFile")
+    import m
+    m.showWindow()
